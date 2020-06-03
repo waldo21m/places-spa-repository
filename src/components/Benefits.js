095450589fd61fc0b39d0234f00bbe12ea7a331f
Eduardo Márquez <eduardo20.3263@gmail.com>
2020-06-02 22:58:44 -0400

    Cambio de diseño en el componente Benefits

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, CardText} from "material-ui/Card";
+import {Card, CardMedia, CardText} from "material-ui/Card";
 import {redA400, lightBlue400, amberA400} from "material-ui/styles/colors";
 
 export default class Benefits extends React.Component {
@@ -8,48 +8,40 @@ export default class Benefits extends React.Component {
       <div className="row">
         <div className="col-xs-12 col-sm-4 Benefit">
           <Card className="Header-Benefit">
-            <CardText>
-              <div className="row">
-                <div className="Header-Benefit-image" style={{"backgroundColor": redA400}}>
-                  <img src={process.env.PUBLIC_URL + "/images/heart.svg"} alt="Heart"/>
-                </div>
-                <div className="Header-Benefit-content">
-                  <h3>Calificaciones con emociones</h3>
-                  <p>Califica tus lugares con experiencias, no con números</p>
-                </div>
+            <CardMedia style={{"backgroundColor": redA400}}>
+              <div className="Header-Benefit-image">
+                <img src={process.env.PUBLIC_URL + "/images/heart.svg"} alt="Heart"/>
               </div>
+            </CardMedia>
+            <CardText>
+              <h3>Calificaciones con emociones</h3>
+              <p>Califica tus lugares con experiencias, no con números</p>
             </CardText>
           </Card>
         </div>
         <div className="col-xs-12 col-sm-4 Benefit">
           <Card className="Header-Benefit">
-            <CardText>
-              <div className="row">
-                <div className="Header-Benefit-image"
-                     style={{"backgroundColor": lightBlue400}}>
-                  <img src={process.env.PUBLIC_URL + "/images/wifi.png"} alt="No Internet"/>
-                </div>
-                <div className="Header-Benefit-content">
-                  <h3>¿Sin internet? Sin problemas</h3>
-                  <p>Places funciona sin internet o en conexiones lentas</p>
-                </div>
+            <CardMedia style={{"backgroundColor": lightBlue400}}>
+              <div className="Header-Benefit-image">
+                <img src={process.env.PUBLIC_URL + "/images/wifi.png"} alt="No Internet"/>
               </div>
+            </CardMedia>
+            <CardText>
+              <h3>¿Sin internet? Sin problemas</h3>
+              <p>Places funciona sin internet o en conexiones lentas</p>
             </CardText>
           </Card>
         </div>
         <div className="col-xs-12 col-sm-4 Benefit">
           <Card className="Header-Benefit">
-            <CardText>
-              <div className="row">
-                <div className="Header-Benefit-image"
-                     style={{"backgroundColor": amberA400}}>
-                  <img src={process.env.PUBLIC_URL + "/images/star.png"} alt="Star"/>
-                </div>
-                <div className="Header-Benefit-content">
-                  <h3>Tus lugares favoritos</h3>
-                  <p>Define tu lista de sitios favoritos</p>
-                </div>
+            <CardMedia style={{"backgroundColor": amberA400}}>
+              <div className="Header-Benefit-image">
+                <img src={process.env.PUBLIC_URL + "/images/star.png"} alt="Star"/>
               </div>
+            </CardMedia>
+            <CardText>
+              <h3>Tus lugares favoritos</h3>
+              <p>Define tu lista de sitios favoritos</p>
             </CardText>
           </Card>
         </div>
